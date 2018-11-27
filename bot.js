3f67d6f3fd46d8d8bad1c1657647f4a0f81bd106
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-11-27 17:46:35 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js")
+const mohamed = new Discord.Client();
+
+
+
+
+client.on('ready', () => {
+    client.channels.find(c => c.id === '516304174229618695').join();
+});
+
+client.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("516302538367303711");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`**-# Welcome To, Tree Center. :cloud: :wine_glass:  **`), 4000)        
+}
+});
+
+client.login(process.env.BOT_TOKEN); 
