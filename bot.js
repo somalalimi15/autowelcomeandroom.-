dbda3e14d9c05c4264e2c53c3ef24ce58fcc9f93
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-11-27 20:16:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,10 @@
 const Discord = require("discord.js")
 const client = new Discord.Client();
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
 
-
-
-
-client.on('ready', () => {
-    client.channels.find(c => c.id === '516304174229618695').join();
-});
 
 client.on('guildMemberAdd', member => {
 const mohamed= member.guild.channels.get("516302538367303711");
@@ -15,5 +13,38 @@ if(mohamed) {
 setTimeout(() => mohamed.send(`**-# Welcome To, Tree Center. :cloud: :wine_glass:  **`), 4000)        
 }
 });
+
+client2.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("516302538367303711");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`**-# Welcome To, Tree Center. :cloud: :wine_glass:  **`), 4000)        
+}
+});
+
+client3.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("516302538367303711");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`**-# Welcome To, Tree Center. :cloud: :wine_glass:  **`), 4000)        
+}
+});
+
+client4.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("516302538367303711");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`**-# Welcome To, Tree Center. :cloud: :wine_glass:  **`), 4000)        
+}
+});
+
+client5.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("516302538367303711");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`**-# Welcome To, Tree Center. :cloud: :wine_glass:  **`), 4000)        
+}
+});
+
 
 client.login(process.env.BOT_TOKEN); 
